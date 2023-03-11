5e39adce6bdc569ac0d8716e5a332f7661bff5e0
Skuld <ry-work@bk.ru>
2023-03-11 19:04:18 +0300

    Создал главное окно. Пока тестирую

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    pass
+import sys
+from PyQt6 import QtWidgets, uic
+
+from MainWindow import Ui_MainWindow
 
 
-if __name__ == '__main__':
-    main()
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
